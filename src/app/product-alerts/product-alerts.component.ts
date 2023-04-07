ee23bee58bb753d43864f37ecc9416ce265ea659
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2023-04-07 16:20:48 +0500

    Pass data to a parent component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -8,5 +8,6 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent {
   @Input() product!: Product;
+  @Output() notify = new EventEmitter();
 
 }
