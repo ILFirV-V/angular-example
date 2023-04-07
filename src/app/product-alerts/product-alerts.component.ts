896de5ad605970cbe0d581f8a5b01ba1e19faee6
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2023-04-07 16:15:40 +0500

    Pass data to a child component

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.scss']
+})
+export class ProductAlertsComponent {
+  @Input() product!: Product;
+
+}
