ed33d1fc9b274cf322e060f5228d5652c8fffffe
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2023-04-08 11:12:02 +0500

    исправление некоторых ошибок

--- src/app/pages/shipping/shipping.component.ts
+++ src/app/pages/shipping/shipping.component.ts
@@ -5,7 +5,7 @@ import {Observable} from "rxjs";
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.css']
+  styleUrls: ['./shipping.component.scss']
 })
 export class ShippingComponent implements OnInit {
   constructor(private cartService: CartService) { }
