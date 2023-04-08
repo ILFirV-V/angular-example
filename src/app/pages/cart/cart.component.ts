ef42ba79e4fd023d8ea438986a2f7babb66b9cf5
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2023-04-08 10:46:31 +0500

    Display the cart items

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CartService} from "../../services/cart.service";
+import {Product} from "../../interfasces/products";
+
 
 @Component({
   selector: 'app-cart',
@@ -6,6 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  items: Product[] = this.cartService.getItems();
 
+  constructor(
+    private cartService: CartService
+  ) { }
 
 }
